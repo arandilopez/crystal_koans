8cceffd9494f223cc6acc1805c4f7dff08c9c5d6
Arandi Lopez <arandilopez.93@gmail.com>
2017-11-13 00:10:37 -0600

    Better fill in helpers

--- src/crystal_koans.cr
+++ src/crystal_koans.cr
@@ -4,19 +4,11 @@ module CrystalKoans
   class FillMeInException < Exception
   end
 
-  def __s(value = "FILL ME IN")
+  def __(value : (Object) = "FILL ME IN")
     return value
   end
 
-  def __b(value = false)
-    return value
-  end
-
-  def __i(value = 99_999)
-    return value
-  end
-
-  def ___(value = FillMeInException)
+  def ___(value : (Class) = FillMeInException)
     return value
   end
 end
