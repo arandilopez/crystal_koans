ee89c40f5cfcdbf02f8ba5f3c3b7532d82fcc199
Arandi Lopez <arandilopez.93@gmail.com>
2017-11-12 22:42:32 -0600

    abouts specs

---
+++ spec/about_specs.cr
@@ -0,0 +1,29 @@
+
+describe "Specs" do
+  # We shall contemplate truth by testing reality, via matchers.
+  it "should test the truth" do
+    true.should be_true
+    false.should be_false
+  end
+
+  # To understand reality, we must compare our expectations against
+  # reality.
+  it "should test equality" do
+    expected_value = 2
+    actual_value = 1 + 1
+    are_equal = expected_value == actual_value
+    are_equal.should be_true
+  end
+
+  # Some ways of asserting equality are better than others.
+  it "should test equality is a better way" do
+    expected_value = 2
+    actual_value = 1 + 1
+    expected_value.should eq actual_value
+  end
+
+  # Sometimes we will ask you to fill in the values
+  it "should ask you to fill in value" do
+    __i(2).should eq 1 + 1
+  end
+end
---
+++ spec/path_to_enlightenment_spec.cr
@@ -0,0 +1,5 @@
+require "./spec_helper"
+
+# The path to Crystal Enlightenment starts with the following:
+require "./about_specs"
+require "./about_nil"
